35492706f50db27ca27cf7fe33adeff3c3b88da9
Lavine Kim <jhkim4@bistel.com>
2021-05-29 14:44:08 +0900

    BinarySearch_Loop

--- Algorithm_note/Search/BinarySearch_Loop.py
+++ Algorithm_note/Search/BinarySearch_Loop.py
@@ -1,19 +1,20 @@
 def binary_search(array, target, start, end):
-  while start <= end:
-    mid = (start+end) // 2
-    # 찾은 경우 중간점 인덱스 반환
-    if array[mid] == target:
-      return mid
-    elif array[mid] > target:
-      return binary_search(array, target, start, mid-1)
-    else:
-      return binary_search(array, target, mid+1, end)
+    while start <= end:
+        mid = (start + end) // 2
+        # 찾은 경우 중간점 인덱스 반환
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            return binary_search(array, target, start, mid - 1)
+        else:
+            return binary_search(array, target, mid + 1, end)
+
 
 n, target = list(map(int, input().split()))
 array = list(map(int, input().split()))
 
-result = binary_search(array, target, 0, n-1)
+result = binary_search(array, target, 0, n - 1)
 if result == None:
-  print("원소가 존재하지 않습니다")
+    print("원소가 존재하지 않습니다")
 else:
-  print(result+1)
+    print(result + 1)
